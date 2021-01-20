112c44738457fa212355aad27267e611a87017e8
HenrietteDusabimana <hdusabim@gmail.com>
2021-01-20 15:01:39 +0200

    added the basic search functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 
 # Create your views here.
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
+
 
 def home(request):
 	orders = Order.objects.all()
@@ -27,22 +30,30 @@ def products(request):
 
 def customer(request, pk_test):
 	customer = Customer.objects.get(id=pk_test)
-
+   
 	orders = customer.order_set.all()
 	order_count = orders.count()
-	context = {'customers':customer, 'orders':orders, 'order_count':order_count}
+
+	myFilter = OrderFilter(request.GET, queryset=orders)
+	orders = myFilter.qs
+	context = {'customers':customer, 'orders':orders, 'order_count':order_count, 'myFilter':myFilter}
 	return render(request, 'accounts/customer.html',context)
 
-def createOrder(request):
-	form = OrderForm()
+def createOrder(request, pk):
+	OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10)
+
+	customer = Customer.objects.get(id=pk)
+	formset = OrderFormSet(queryset=Order.objects.none(),instance=customer)
+	# form = OrderForm(initial={'customer':customer})
 	if request.method == 'POST':
 		#print('Printing POST:', request.POST)
-		form = OrderForm(request.POST)
-		if form.is_valid():
-			form.save()
+		# form = OrderForm(request.POST)
+		formset = OrderFormSet(request.POST, instance=customer)
+		if formset.is_valid():
+			formset.save()
 			return redirect('/')
 
-	context = {'form':form}
+	context = {'formset':formset}
 	return render(request, 'accounts/order_form.html', context)
 
 def updateOrder(request, pk):
--- crm/settings.py
+++ crm/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'accounts'
+    'accounts',
+
+    'django_filters',
 
 ]
 
