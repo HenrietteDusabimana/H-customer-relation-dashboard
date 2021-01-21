2e3c8c8f1c0ec24118322ac5878200d12445e364
HenrietteDusabimana <hdusabim@gmail.com>
2021-01-21 15:23:20 +0200

    added the user profile

--- accounts/views.py
+++ accounts/views.py
@@ -28,6 +28,9 @@ def registerPage(request):
 
 			group = Group.objects.get(name='customer')
 			user.groups.add(group)
+			Customer.objects.create(
+				user=user,
+				)
 
 			messages.success(request, 'Account was created for ' + username)
 
@@ -77,13 +80,23 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+
 def userPage(request):
-	context = {}
+	orders = request.user.customer.order_set.all()
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	context = {'orders':orders, 'total_orders':total_orders,'delivered':delivered,
+	'pending':pending }
 	return render(request, 'accounts/user.html', context)
 
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
+
 def products(request):
 	products = Product.objects.all()
 
@@ -91,6 +104,7 @@ def products(request):
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
+
 def customer(request, pk_test):
 	customer = Customer.objects.get(id=pk_test)
 
