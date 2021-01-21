3b4e87f2e3bef3ddde5712644b432cdfa28ab7fb
HenrietteDusabimana <hdusabim@gmail.com>
2021-01-21 17:23:38 +0200

    added the profile image for customers

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,18 +1,23 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 from django.contrib.auth.models import User
+from django import forms
 
-from .models import Order
+from .models import *
 
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
 	class Meta:
-		model = Order 
+		model = Order
 		fields = '__all__'
 
 
 class CreateUserForm(UserCreationForm):
 	class Meta:
-		model = User 
-		fields = ['username', 'email', 'password1', 'password2' ]
+		model = User
+		fields = ['username', 'email', 'password1', 'password2']
--- crm/settings.py
+++ crm/settings.py
@@ -130,3 +130,5 @@ MEDIA_URL = '/Images/'
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'static')
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
