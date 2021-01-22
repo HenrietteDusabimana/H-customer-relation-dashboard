5964f076e17764b3585b1f82a78c119a319e1d06
HenrietteDusabimana <hdusabim@gmail.com>
2021-01-22 10:46:40 +0200

    added signals to listen to a new user created that is like triggers, and basically uses apps.py to be impacted

--- accounts/views.py
+++ accounts/views.py
@@ -27,10 +27,7 @@ def registerPage(request):
 			username = form.cleaned_data.get('username')
 
 			group = Group.objects.get(name='customer')
-			user.groups.add(group)
-			Customer.objects.create(
-				user=user,
-				)
+			
 
 			messages.success(request, 'Account was created for ' + username)
 
